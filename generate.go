3b1c9b02349fc26e0799ed25672869864f5802ef
Googler <noreply@google.com>
2025-07-18 09:58:26 -0700

    Project import generated by Copybara.
    PiperOrigin-RevId: 784614351

--- generate.go
+++ generate.go
@@ -1,4 +1,4 @@
-// Copyright 2024 Google LLC
+// Copyright 2025 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,6 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package testing
+package xlapjrt
 
-//go:generate bash -c "go tool cgo -exportheader testing.cgo.h -- `echo $CGO_CFLAGS` testing.go"
+//go:generate go tool packager --gx_package_module=github.com/gx-org/xlapjrt/gx
